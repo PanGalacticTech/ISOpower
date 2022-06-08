15c60f12ab742a7ee2e4b719520b99258203ec09
MANAPOTY Bhavith <bmanapot@ed.ac.uk>
2022-06-08 16:41:05 +0100

    changes to ACS712 Current sense

--- Firmware/AtMega_test_firmware/currentSense.h
+++ Firmware/AtMega_test_firmware/currentSense.h
@@ -11,6 +11,14 @@ int16_t bus12v_sample;
 
 int16_t bus5v_sample;
 
+int16_t adcVoltage;
+
+int16_t currentValue;
+
+//Constant Values for ACS712-30A (Can change for different models of board)
+int sensitivity = 66;
+int offsetVoltage = 2500;
+
 
 
 autoDelay sampleDelay;
@@ -27,5 +35,9 @@ void sampleADCs() {
 
 
 void ADCtoVoltage(int16_t adcValue){
-  
+  adcVoltage = (adcValue / 1023) * 5000;
+}
+
+void voltageToCurrent(int16_t adcVoltage){
+  currentValue = ((adcVoltage - offsetVoltage) / sensitivity);
 }
